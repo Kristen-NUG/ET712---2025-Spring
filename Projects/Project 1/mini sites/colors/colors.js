25ea93428721f67b0c917d9aeed6d52c32341247
Kristen <kristen.grant41@student.qcc.cuny.edu>
2025-03-31 17:14:45 -0400

    a

--- Projects/Project 1/mini sites/colors/colors.js
+++ Projects/Project 1/mini sites/colors/colors.js
@@ -1,13 +1,21 @@
-let colour = document.querySelectorAll('.colour')
+let colour = document.querySelector('.colour')
 function colorChange(element){
-    let intro = document.querySelector('intro');
+    let intro = element.querySelector('intro');
     for (let i = 0; i < intro.length; i++){
         let tro = intro[i];
         let randomColor = .2 + Math.random() * 1;
-        tro.style.setPropperty("--duration", randomColor+"s");
+        tro.style.setProperty("--duration", randomColor+"s");
 
     }
     setInitialRandomColor(intro);
-    colour.addEventListener('animationiteration', Color, true)
+    colour.addEventListener('animationiteration', changeColor, true)
 }
 colorChange(colour);
+function setInitialRandomColor(elements){
+    for (let i = 0; i < colour.length; i++){
+        let tro = elements[i];
+        tro.style.setProperty("--colorA", getRandomColor().value);
+        tro.style.setProperty("--colorB", getRandomColor().value);
+
+    }
+}
