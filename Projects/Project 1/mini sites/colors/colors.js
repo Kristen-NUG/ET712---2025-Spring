5390a25b0f5a8e3cc4f903c79dc23d58965a8386
Kristen <kristen.grant41@student.qcc.cuny.edu>
2025-03-31 18:36:30 -0400

    a

--- Projects/Project 1/mini sites/colors/colors.js
+++ Projects/Project 1/mini sites/colors/colors.js
@@ -4,17 +4,32 @@ function getRandomColor() {
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`;
 }
-function setRandomColors() {
+function applyRandomColorAnimation() {
     const introElement = document.querySelector('.intro');
-    const colorA = getRandomColor();
-    const colorB = getRandomColor();
-    const duration = (2 + Math.random() * 3) + 's'; 
+    const color1 = getRandomColor();
+    const color2 = getRandomColor();
+    const color3 = getRandomColor();
+    const color4 = getRandomColor();
+    
 
-    introElement.style.setProperty('--colorA', colorA);
-    introElement.style.setProperty('--colorB', colorB);
-    introElement.style.setProperty('--duration', duration);
+    const styleSheet = document.styleSheets[0];
+    const animationName = 'dynamicColorCycle';
+    const keyframes = `
+        @keyframes ${animationName} {
+            0% { color: ${color1}; }
+            25% { color: ${color2}; }
+            50% { color: ${color3}; }
+            75% { color: ${color4}; }
+            100% { color: ${color1}; }
+        }`;
+    
+        for (let i = styleSheet.cssRules.length - 1; i >= 0; i--) {
+        if (styleSheet.cssRules[i].name === animationName) {
+            styleSheet.deleteRule(i);
+        }
+    }
+    styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
+
+    introElement.style.animation = `${animationName} 5s infinite linear`;
 }
-setRandomColors();
-
-const introElement = document.querySelector('.intro');
-introElement.addEventListener('animationiteration', setRandomColors);
+applyRandomColorAnimation()
