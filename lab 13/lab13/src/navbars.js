8828abd7067f9450691d75840a907903350a4d8e
Kristen-NUG <kristen.grant41@student.qcc.cuny.edu>
2025-04-22 19:09:10 +0000

    a

---
+++ lab 13/lab13/src/navbars.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbars = function(){
+    return(
+        <>
+         <header><h1>Navigation Bars</h1></header>
+        <nav>
+            <section>
+                <Link>QCC</Link>
+            </section>
+            <section>
+                <button><Link to="/home">Home</Link></button>
+                <button><Link to="/about">About Us</Link></button>
+                <button><Link to="/contact">Contact Us</Link></button>
+            </section>
+        </nav>
+        </>
+    )
+}
+
+export default Navbars
